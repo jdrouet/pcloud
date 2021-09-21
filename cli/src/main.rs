a9446197b6163db13236803229c6a6d1179139fd
Jérémie Drouet <jdrouet@users.noreply.github.com>
2021-09-21 21:36:28 +0200

    feat(cli): load config from file (#22)
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,20 +1,42 @@
+mod config;
 mod file;
 mod folder;
 
 use clap::Clap;
 use pcloud::http::HttpClient;
+use std::path::PathBuf;
 
 #[derive(Clap)]
-enum Command {
+struct Command {
+    #[clap(short, long)]
+    config: Option<PathBuf>,
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+impl Command {
+    fn config(&self) -> PathBuf {
+        if let Some(ref cfg) = self.config {
+            cfg.clone()
+        } else if let Some(cfg_dir) = dirs::config_dir() {
+            cfg_dir.join("pcloud.json")
+        } else {
+            PathBuf::from(".pcloud.json")
+        }
+    }
+}
+
+#[derive(Clap)]
+enum SubCommand {
     Folder(folder::Command),
     File(file::Command),
 }
 
 impl Command {
     async fn execute(&self, pcloud: HttpClient) {
-        match self {
-            Self::Folder(sub) => sub.execute(pcloud).await,
-            Self::File(sub) => sub.execute(pcloud).await,
+        match &self.subcmd {
+            SubCommand::Folder(sub) => sub.execute(pcloud).await,
+            SubCommand::File(sub) => sub.execute(pcloud).await,
         }
     }
 }
@@ -24,6 +46,8 @@ async fn main() {
     env_logger::init();
 
     let cmd = Command::parse();
-    let pcloud = HttpClient::from_env();
+    let pcloud = config::Config::from_path(&cmd.config())
+        .map(|cfg| cfg.build())
+        .unwrap_or_else(|_| HttpClient::from_env());
     cmd.execute(pcloud).await;
 }

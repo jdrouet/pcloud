f9b977a1e4fab311507a3314acb5f9a252747408
Jérémie Drouet <jeremie.drouet@gmail.com>
2022-04-08 21:08:03 +0200

    feat(cli): add overrides flag when downloading file
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- cli/src/file/download.rs
+++ cli/src/file/download.rs
@@ -1,18 +1,28 @@
 use clap::Parser;
 use pcloud::http::HttpClient;
-use std::fs::File;
+use std::fs::OpenOptions;
 use std::path::PathBuf;
 
 #[derive(Parser)]
 pub struct Command {
+    /// Overrides an existing file
+    #[clap(long)]
+    overrides: bool,
+    /// Remote file id
     file_id: u64,
+    /// Output path for the file
     path: PathBuf,
 }
 
 impl Command {
     #[tracing::instrument(skip_all)]
     pub async fn execute(&self, pcloud: HttpClient) {
-        let file = File::create(&self.path).expect("unable to create file");
+        let file = OpenOptions::new()
+            .create_new(!self.overrides)
+            .create(true)
+            .truncate(true)
+            .open(&self.path)
+            .expect("unable to create file");
         match pcloud.download_file(self.file_id, file).await {
             Ok(res) => {
                 tracing::info!("file downloaded: {}", res);

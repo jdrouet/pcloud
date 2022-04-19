aeae58004580a340d28f07e123adb1568c902a23
Jérémie Drouet <jeremie.drouet@gmail.com>
2022-04-19 11:18:21 +0200

    refactor(lib): create download command
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- lib/src/file/download.rs
+++ lib/src/file/download.rs
@@ -1,27 +1,32 @@
 use super::FileIdentifier;
 use crate::error::Error;
 use crate::http::HttpClient;
+use crate::prelude::Command;
 use std::io::Write;
 
-impl HttpClient {
-    /// Download a file
-    ///
-    /// # Arguments
-    ///
-    /// * `identifier` - ID or path to the file to download.
-    /// * `write` - Where to write the file.
-    ///
-    #[tracing::instrument(skip(self, write))]
-    pub async fn download_file<I: Into<FileIdentifier> + std::fmt::Debug, W: Write>(
-        &self,
-        identifier: I,
-        mut write: W,
-    ) -> Result<usize, Error> {
-        let link = self.get_link_file(identifier).await?;
-        let mut req = self.client.get(&link).send().await?;
+#[derive(Debug)]
+pub struct FileDownloadCommand<W> {
+    identifier: FileIdentifier,
+    writer: W,
+}
+
+impl<W: Write> FileDownloadCommand<W> {
+    pub fn new(identifier: FileIdentifier, writer: W) -> Self {
+        Self { identifier, writer }
+    }
+}
+
+#[async_trait::async_trait(?Send)]
+impl<W: Write> Command for FileDownloadCommand<W> {
+    type Output = usize;
+    type Error = Error;
+
+    async fn execute(mut self, client: &HttpClient) -> Result<Self::Output, Self::Error> {
+        let link = client.get_link_file(self.identifier).await?;
+        let mut req = client.client.get(&link).send().await?;
         let mut size = 0;
         while let Some(chunk) = req.chunk().await? {
-            size += write.write(chunk.as_ref()).map_err(Error::Download)?;
+            size += self.writer.write(chunk.as_ref()).map_err(Error::Download)?;
         }
         Ok(size)
     }

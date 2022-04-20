37c56e7ee74ee6ec3ad4c4ecf111b6bf971aa4da
Jérémie Drouet <jeremie.drouet@gmail.com>
2022-04-20 10:03:47 +0200

    refactor(lib): create file copy command
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- lib/src/file/copy.rs
+++ lib/src/file/copy.rs
@@ -3,15 +3,16 @@ use crate::binary::{BinaryClient, Value as BinaryValue};
 use crate::entry::File;
 use crate::error::Error;
 use crate::http::HttpClient;
+use crate::prelude::Command;
 use crate::request::Response;
 
 #[derive(Debug)]
-pub struct Params {
+pub struct FileCopyCommand {
     file_id: u64,
     to_folder_id: u64,
 }
 
-impl Params {
+impl FileCopyCommand {
     pub fn new(file_id: u64, to_folder_id: u64) -> Self {
         Self {
             file_id,
@@ -34,11 +35,14 @@ impl Params {
     }
 }
 
-impl HttpClient {
-    #[tracing::instrument(skip(self))]
-    pub async fn copy_file(&self, params: &Params) -> Result<File, Error> {
-        let result: Response<FileResponse> = self
-            .get_request("copyfile", &params.to_http_params())
+#[async_trait::async_trait(?Send)]
+impl Command for FileCopyCommand {
+    type Output = File;
+    type Error = Error;
+
+    async fn execute(self, client: &HttpClient) -> Result<Self::Output, Self::Error> {
+        let result: Response<FileResponse> = client
+            .get_request("copyfile", &self.to_http_params())
             .await?;
         result.payload().map(|item| item.metadata)
     }
@@ -46,7 +50,7 @@ impl HttpClient {
 
 impl BinaryClient {
     #[tracing::instrument(skip(self))]
-    pub fn copy_file(&mut self, params: &Params) -> Result<File, Error> {
+    pub fn copy_file(&mut self, params: &FileCopyCommand) -> Result<File, Error> {
         let result = self.send_command("copyfile", &params.to_binary_params())?;
         let result: Response<FileResponse> = serde_json::from_value(result)?;
         result.payload().map(|item| item.metadata)

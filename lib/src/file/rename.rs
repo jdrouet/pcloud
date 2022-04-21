306278469b0867e2bfff6b0c4ce3a102b0b9241c
Jérémie Drouet <jeremie.drouet@gmail.com>
2022-04-21 13:12:06 +0200

    refactor(lib): create file rename binary command
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- lib/src/file/rename.rs
+++ lib/src/file/rename.rs
@@ -4,7 +4,7 @@ use crate::entry::File;
 use crate::error::Error;
 use crate::folder::FolderIdentifier;
 use crate::http::HttpClient;
-use crate::prelude::HttpCommand;
+use crate::prelude::{BinaryCommand, HttpCommand};
 use crate::request::Response;
 
 #[derive(Debug)]
@@ -30,6 +30,19 @@ impl FileMoveCommand {
         });
         res
     }
+
+    fn to_binary_params(&self) -> Vec<(&str, BinaryValue)> {
+        let mut res = vec![];
+        res.push(match &self.from {
+            FileIdentifier::FileId(id) => ("fileid", BinaryValue::Number(*id)),
+            FileIdentifier::Path(value) => ("path", BinaryValue::Text(value.to_string())),
+        });
+        res.push(match &self.to {
+            FolderIdentifier::FolderId(id) => ("tofolderid", BinaryValue::Number(*id)),
+            FolderIdentifier::Path(value) => ("topath", BinaryValue::Text(value.to_string())),
+        });
+        res
+    }
 }
 
 #[async_trait::async_trait(?Send)]
@@ -40,6 +53,16 @@ impl HttpCommand for FileMoveCommand {
         let result: Response<FileResponse> = client
             .get_request("renamefile", &self.to_http_params())
             .await?;
+        result.payload().map(|item| item.metadata)
+    }
+}
+
+impl BinaryCommand for FileMoveCommand {
+    type Output = File;
+
+    fn execute(self, client: &mut BinaryClient) -> Result<Self::Output, Error> {
+        let result = client.send_command("renamefile", &self.to_binary_params())?;
+        let result: Response<FileResponse> = serde_json::from_value(result)?;
         result.payload().map(|item| item.metadata)
     }
 }
@@ -64,6 +87,16 @@ impl FileRenameCommand {
         res.push(("toname", self.name.to_string()));
         res
     }
+
+    fn to_binary_params(&self) -> Vec<(&str, BinaryValue)> {
+        let mut res = vec![];
+        res.push(match &self.identifier {
+            FileIdentifier::FileId(id) => ("fileid", BinaryValue::Number(*id)),
+            FileIdentifier::Path(value) => ("path", BinaryValue::Text(value.to_string())),
+        });
+        res.push(("toname", BinaryValue::Text(self.name.to_string())));
+        res
+    }
 }
 
 #[async_trait::async_trait(?Send)]
@@ -78,77 +111,11 @@ impl HttpCommand for FileRenameCommand {
     }
 }
 
-#[derive(Debug)]
-pub enum Params {
-    Move {
-        from: FileIdentifier,
-        to: FolderIdentifier,
-    },
-    Rename {
-        from: FileIdentifier,
-        to_name: String,
-    },
-}
+impl BinaryCommand for FileRenameCommand {
+    type Output = File;
 
-impl Params {
-    pub fn new_move<File: Into<FileIdentifier>, Folder: Into<FolderIdentifier>>(
-        from: File,
-        to: Folder,
-    ) -> Self {
-        Self::Move {
-            from: from.into(),
-            to: to.into(),
-        }
-    }
-
-    pub fn new_rename<I: Into<FileIdentifier>, S: Into<String>>(from: I, to_name: S) -> Self {
-        Self::Rename {
-            from: from.into(),
-            to_name: to_name.into(),
-        }
-    }
-
-    pub fn to_binary_params(&self) -> Vec<(&str, BinaryValue)> {
-        let mut res = vec![];
-        match self {
-            Self::Move { from, to } => {
-                match from {
-                    FileIdentifier::FileId(id) => {
-                        res.push(("fileid", BinaryValue::Number(*id)));
-                    }
-                    FileIdentifier::Path(value) => {
-                        res.push(("path", BinaryValue::Text(value.to_string())));
-                    }
-                };
-                match to {
-                    FolderIdentifier::FolderId(id) => {
-                        res.push(("tofolderid", BinaryValue::Number(*id)));
-                    }
-                    FolderIdentifier::Path(value) => {
-                        res.push(("topath", BinaryValue::Text(value.to_string())));
-                    }
-                };
-            }
-            Self::Rename { from, to_name } => {
-                match from {
-                    FileIdentifier::FileId(id) => {
-                        res.push(("fileid", BinaryValue::Number(*id)));
-                    }
-                    FileIdentifier::Path(value) => {
-                        res.push(("path", BinaryValue::Text(value.to_string())));
-                    }
-                };
-                res.push(("toname", BinaryValue::Text(to_name.to_string())));
-            }
-        }
-        res
-    }
-}
-
-impl BinaryClient {
-    #[tracing::instrument(skip(self))]
-    pub fn rename_file(&mut self, params: &Params) -> Result<File, Error> {
-        let result = self.send_command("renamefile", &params.to_binary_params())?;
+    fn execute(self, client: &mut BinaryClient) -> Result<Self::Output, Error> {
+        let result = client.send_command("renamefile", &self.to_binary_params())?;
         let result: Response<FileResponse> = serde_json::from_value(result)?;
         result.payload().map(|item| item.metadata)
     }

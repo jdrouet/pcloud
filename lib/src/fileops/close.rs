6a917006c37da8b93db3c4b4719df68bb8274869
Jérémie Drouet <jeremie.drouet@gmail.com>
2022-04-21 17:18:32 +0200

    refactor(lib): create fileops close binary command
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- lib/src/fileops/close.rs
+++ lib/src/fileops/close.rs
@@ -1,12 +1,25 @@
 use crate::binary::{BinaryClient, Value as BinaryValue};
 use crate::error::Error;
+use crate::prelude::BinaryCommand;
 use crate::request::Response;
 
-impl BinaryClient {
-    #[tracing::instrument(skip(self))]
-    pub fn file_close(&mut self, fd: u64) -> Result<(), Error> {
-        let params = vec![("fd", BinaryValue::Number(fd))];
-        let res = self.send_command("file_close", &params)?;
+#[derive(Debug)]
+pub struct FileCloseCommand {
+    fd: u64,
+}
+
+impl FileCloseCommand {
+    pub fn new(fd: u64) -> Self {
+        Self { fd }
+    }
+}
+
+impl BinaryCommand for FileCloseCommand {
+    type Output = ();
+
+    fn execute(self, client: &mut BinaryClient) -> Result<Self::Output, Error> {
+        let params = vec![("fd", BinaryValue::Number(self.fd))];
+        let res = client.send_command("file_close", &params)?;
         let res: Response<()> = serde_json::from_value(res)?;
         res.payload()
     }

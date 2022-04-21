582ae78d8c5b4929f2fd3a6fa078d74537ec6d32
Jérémie Drouet <jeremie.drouet@gmail.com>
2022-04-21 11:17:15 +0200

    refactor(lib): create BinaryCommand trait
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- lib/src/prelude.rs
+++ lib/src/prelude.rs
@@ -1,3 +1,4 @@
+use crate::binary::BinaryClient;
 use crate::error::Error;
 use crate::http::HttpClient;
 
@@ -6,4 +7,10 @@ pub trait HttpCommand {
     type Output;
 
     async fn execute(self, client: &HttpClient) -> Result<Self::Output, Error>;
+}
+
+pub trait BinaryCommand {
+    type Output;
+
+    fn execute(self, client: &mut BinaryClient) -> Result<Self::Output, Error>;
 }

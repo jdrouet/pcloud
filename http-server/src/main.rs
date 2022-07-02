8788cc2795c338e39e885401ad130ebb1ad4162a
Jérémie Drouet <jdrouet@users.noreply.github.com>
2022-07-02 12:54:50 +0200

    feat(http-server): load root folder from environment variable (#55)
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -14,11 +14,18 @@ pub struct Config {
     // The port to bind the server
     #[clap(long, default_value = "8080", env = "PORT")]
     port: u16,
+    // The root folder
+    #[clap(long, default_value = "/", env = "ROOT_FOLDER")]
+    root_folder: String,
 }
 
 impl Config {
     pub fn binding(&self) -> String {
         format!("{}:{}", self.host, self.port)
+    }
+
+    pub fn root_folder(&self) -> crate::handler::RootFolder {
+        crate::handler::RootFolder::new(self.root_folder.clone())
     }
 }
 
@@ -30,10 +37,12 @@ async fn main() -> std::io::Result<()> {
     let client = HttpClientBuilder::from_env()
         .build()
         .expect("couldn't build client");
+    let client = web::Data::new(client);
+    let root = web::Data::new(args.root_folder());
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(client.clone()))
-            .app_data(web::Data::new(handler::RootFolder::from_env()))
+            .app_data(client.clone())
+            .app_data(root.clone())
             .service(web::resource("{tail:.*}").to(handler::handle))
     })
     .bind(args.binding())?

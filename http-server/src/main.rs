1c0622941fa499c806ef93dee13ed772fb37de21
Jérémie Drouet <jeremie.drouet@gmail.com>
2022-07-02 10:34:50 +0000

    feat(http-server): add clap to configure server
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -2,19 +2,31 @@ mod handler;
 mod render;
 
 use actix_web::{web, App, HttpServer};
+use clap::Parser;
 use pcloud::http::HttpClientBuilder;
 
-fn binding() -> String {
-    let host = std::env::var("HOST").unwrap_or_else(|_| "localhost".into());
-    let port = std::env::var("PORT").unwrap_or_else(|_| "8080".into());
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Config {
+    // The host to bind the server
+    #[clap(long, default_value = "localhost", env = "HOST")]
+    host: String,
+    // The port to bind the server
+    #[clap(long, default_value = "8080", env = "PORT")]
+    port: u16,
+}
 
-    format!("{}:{}", host, port)
+impl Config {
+    pub fn binding(&self) -> String {
+        format!("{}:{}", self.host, self.port)
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
 
+    let args = Config::parse();
     let client = HttpClientBuilder::from_env()
         .build()
         .expect("couldn't build client");
@@ -24,7 +36,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(handler::RootFolder::from_env()))
             .service(web::resource("{tail:.*}").to(handler::handle))
     })
-    .bind(binding())?
+    .bind(args.binding())?
     .run()
     .await
 }

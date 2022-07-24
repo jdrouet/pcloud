e11faa0f65086e4c7cf02195e92fdd15825796de
Jérémie Drouet <jeremie.drouet@gmail.com>
2022-07-24 00:00:00 +0000

    fuse: update args to fit what is given by mount

--- fuse/src/main.rs
+++ fuse/src/main.rs
@@ -4,69 +4,91 @@ mod service;
 
 use clap::Parser;
 use fuser::MountOption;
+use std::collections::HashMap;
 use std::path::PathBuf;
+
+// handle fuse mount arguments like
+//
+// pcloud-fuse <folder_id> <path_to_mount> -o rw,blabla,dev,suid
+
+fn as_mount_options<'a>(opts: &HashMap<&'a str, Option<&'a str>>) -> Vec<MountOption> {
+    let mut result = vec![
+        MountOption::AutoUnmount,
+        MountOption::NoExec,
+        MountOption::NoAtime,
+    ];
+    if opts.contains_key("ro") {
+        result.push(MountOption::RO);
+    } else {
+        result.push(MountOption::RW);
+    }
+    result
+}
+
+fn init_log_level<'a>(opts: &HashMap<&'a str, Option<&'a str>>) {
+    let log_level = match opts.get("log_level") {
+        Some(Some(value)) => value.to_string(),
+        _ => "info".to_string(),
+    };
+    tracing_subscriber::fmt()
+        .with_env_filter(log_level)
+        .try_init()
+        .expect("couldn't init logger");
+}
+
+fn get_config_path<'a>(opts: &HashMap<&'a str, Option<&'a str>>) -> PathBuf {
+    match opts.get("config_path") {
+        Some(Some(value)) => PathBuf::from(value),
+        _ => PathBuf::from("/etc/pcloud.conf"),
+    }
+}
 
 #[derive(Parser)]
 #[clap(about, author, version)]
 struct Opts {
-    /// Path to load the configuration file. Default to ~/.config/pcloud.json. If not found, loading from environment.
-    #[clap(short, long)]
-    config: Option<PathBuf>,
-    #[clap(long, default_value = "info")]
-    log_level: String,
     #[clap(long, short)]
-    read_only: bool,
+    options: Option<String>,
+    // TODO use the folder id for the root path
+    folder_id: u64,
     mount_point: PathBuf,
 }
 
 impl Opts {
-    fn config(&self) -> PathBuf {
-        if let Some(ref cfg) = self.config {
-            cfg.clone()
-        } else if let Some(cfg_dir) = dirs::config_dir() {
-            cfg_dir.join("pcloud.json")
-        } else {
-            PathBuf::from(".pcloud.json")
-        }
-    }
-
-    fn set_log_level(&self) {
-        tracing_subscriber::fmt()
-            .with_env_filter(self.log_level.clone())
-            .try_init()
-            .expect("couldn't init logger");
-    }
-
-    fn options(&self) -> Vec<MountOption> {
-        let mut options = vec![
-            MountOption::AutoUnmount,
-            MountOption::NoExec,
-            MountOption::NoAtime,
-        ];
-        if self.read_only {
-            options.push(MountOption::RO);
-        } else {
-            options.push(MountOption::RW);
-        }
-        options
+    fn options<'a>(&'a self) -> HashMap<&'a str, Option<&'a str>> {
+        self.options
+            .as_ref()
+            .map(|inner| {
+                inner
+                    .split(",")
+                    .map(|item| {
+                        if let Some((key, value)) = item.split_once('=') {
+                            (key, Some(value))
+                        } else {
+                            (item, None)
+                        }
+                    })
+                    .collect::<HashMap<_, _>>()
+            })
+            .unwrap_or_default()
     }
 }
 
 fn main() {
     let opts = Opts::parse();
-    opts.set_log_level();
+    let options = opts.options();
+    init_log_level(&options);
 
-    let cfg = config::Config::from_path(&opts.config()).unwrap_or_default();
+    let cfg = config::Config::from_path(&get_config_path(&options)).unwrap_or_default();
     let client = cfg.build().expect("couldn't build client");
 
     let service = service::PCloudService::new(client);
 
-    let options = opts.options();
+    let mnt_opts = as_mount_options(&options);
 
     fuser::mount2(
         fs::PCloudFs::new(service),
         opts.mount_point.to_str().unwrap(),
-        &options,
+        &mnt_opts,
     )
     .expect("couldn't mount");
 }

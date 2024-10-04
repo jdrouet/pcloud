bc118cd6a366c2020b6051eef66e0c8043686f21
Jérémie Drouet <jeremie.drouet@gmail.com>
2024-10-04 19:12:43 +0200

    refactor(cli): go through a queue without a recursive function
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- cli/src/cmd/download.rs
+++ cli/src/cmd/download.rs
@@ -1,35 +1,93 @@
-use std::path::{Path, PathBuf};
+use std::collections::VecDeque;
+use std::path::PathBuf;
 
 use pcloud::client::HttpClient;
 use pcloud::entry::{Entry, File, Folder};
 use pcloud::file::FileIdentifier;
 use pcloud::prelude::HttpCommand;
 
-fn is_empty(path: &Path) -> std::io::Result<bool> {
-    let mut content = std::fs::read_dir(path)?;
-    Ok(content.next().is_none())
+struct DownloadManager<'a> {
+    client: &'a HttpClient,
+    dry_run: bool,
+    skip_existing: bool,
+    queue: VecDeque<(Entry, PathBuf)>,
 }
 
-fn collect_files(folder: &Folder, path: PathBuf) -> Vec<(&File, PathBuf)> {
-    let mut queue = vec![(folder, path)];
-    let mut res = Vec::new();
-    while let Some((folder, path)) = queue.pop() {
-        for item in folder.contents.iter().flat_map(|item| item.iter()) {
-            match item {
-                Entry::Folder(inner) => {
-                    queue.push((inner, path.join(inner.base.name.as_str())));
-                }
-                Entry::File(inner) => {
-                    res.push((inner, path.join(inner.base.name.as_str())));
-                }
-            }
+impl<'a> DownloadManager<'a> {
+    fn new(client: &'a HttpClient, dry_run: bool, skip_existing: bool) -> Self {
+        Self {
+            client,
+            dry_run,
+            skip_existing,
+            queue: VecDeque::with_capacity(1),
         }
     }
-    res
+
+    async fn process_file(&mut self, file: File, target: PathBuf) -> anyhow::Result<()> {
+        tracing::info!("downloading file {:?} to {target:?}", file.base.name);
+        if self.dry_run {
+            return Ok(());
+        }
+        if target.exists() && self.skip_existing {
+            return Ok(());
+        }
+        if let Some(parent) = target.parent() {
+            if !parent.exists() {
+                std::fs::create_dir_all(&parent)?;
+            }
+        }
+        let writer = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(target)?;
+        let ident = FileIdentifier::file_id(file.file_id);
+        pcloud::file::download::FileDownloadCommand::new(ident, writer)
+            .execute(self.client)
+            .await?;
+        Ok(())
+    }
+
+    async fn process_folder(&mut self, folder: Folder, target: PathBuf) -> anyhow::Result<()> {
+        tracing::info!("downloading folder {:?} to {target:?}", folder.base.name);
+        if let Some(children) = folder.contents {
+            for child in children {
+                let new_target = target.join(child.base().name.as_str());
+                self.queue.push_back((child, new_target));
+            }
+        } else {
+            let res = pcloud::folder::list::FolderListCommand::new(folder.folder_id.into())
+                .execute(self.client)
+                .await?;
+            for child in res.contents.into_iter().flatten() {
+                let new_target = target.join(child.base().name.as_str());
+                self.queue.push_back((child, new_target));
+            }
+        }
+        Ok(())
+    }
+
+    async fn run(mut self, entry: Entry, target: PathBuf) -> anyhow::Result<()> {
+        self.queue.push_front((entry, target));
+        while let Some((entry, target)) = self.queue.pop_front() {
+            match entry {
+                Entry::File(file) => self.process_file(file, target).await?,
+                Entry::Folder(folder) => self.process_folder(folder, target).await?,
+            }
+            tracing::info!("{} elements left in the queue", self.queue.len());
+        }
+        Ok(())
+    }
 }
 
 #[derive(clap::Parser)]
 pub(crate) struct Command {
+    /// When enabled, nothing will be really created on disk
+    #[clap(long)]
+    dry_run: bool,
+    /// When enabled, if the file already exists, the download is skipped.
+    #[clap(long)]
+    skip_existing: bool,
+
     /// Remote path to the file or directory to download
     remote_path: String,
     /// Local path to download the file or directory to
@@ -41,7 +99,6 @@ impl Command {
         // assuming it's doing a `ls` on a folder at first
         let folder_id = pcloud::folder::FolderIdentifier::path(&self.remote_path);
         let folder_res = pcloud::folder::list::FolderListCommand::new(folder_id)
-            .with_recursive(true)
             .execute(client)
             .await;
         match folder_res {
@@ -58,64 +115,23 @@ impl Command {
         }
     }
 
-    async fn download_file(
-        &self,
-        client: &HttpClient,
-        file: &File,
-        target: PathBuf,
-    ) -> anyhow::Result<()> {
-        let writer = std::fs::OpenOptions::new()
-            .write(true)
-            .create_new(true)
-            .open(target)?;
-        let ident = FileIdentifier::file_id(file.file_id);
-        pcloud::file::download::FileDownloadCommand::new(ident, writer)
-            .execute(client)
-            .await?;
-        Ok(())
-    }
-
-    async fn download_folder(
-        &self,
-        client: &HttpClient,
-        folder: &Folder,
-        target: PathBuf,
-    ) -> anyhow::Result<()> {
-        let files = collect_files(folder, target);
-        for (file, path) in files.into_iter() {
-            if let Some(parent) = path.parent() {
-                if !parent.exists() {
-                    std::fs::create_dir_all(parent)?;
-                }
-            }
-            self.download_file(client, file, path).await?;
-        }
-        Ok(())
-    }
-
     pub(crate) async fn execute(self, client: &HttpClient) -> anyhow::Result<()> {
         let result = self.fetch_entry(client).await?;
+        let manager = DownloadManager::new(client, self.dry_run, self.skip_existing);
         match result {
             Entry::File(file) => {
                 let target = if self.local_path.exists() && self.local_path.is_dir() {
                     self.local_path.join(file.base.name.as_str())
-                } else if self.local_path.exists() && self.local_path.is_file() {
-                    return Err(anyhow::anyhow!("the target file already exists"));
                 } else {
                     self.local_path.clone()
                 };
-                self.download_file(client, &file, target).await?;
+                manager.run(Entry::File(file), target).await?;
             }
             Entry::Folder(folder) => {
                 if self.local_path.is_file() {
                     return Err(anyhow::anyhow!("the provided local path is a file"));
                 }
-                let local = if self.local_path.exists() && !is_empty(&self.local_path)? {
-                    self.local_path.join(folder.base.name.as_str())
-                } else {
-                    self.local_path.clone()
-                };
-                self.download_folder(client, &folder, local).await?;
+                manager.run(Entry::Folder(folder), self.local_path).await?;
             }
         }
         Ok(())
